730264d58dc62a70597a9568727dad63abf08a11
Caz <kennedycarrick@icloud.com>
2026-05-11 13:56:14 +0100

    Main updated with DB

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,32 @@
+from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Depends
-from sqlmodel import Session, select
-from db.connection import create_db_and_tables, get_session
-from db.models import Character, Item
+from db.connection import init_db
+from db.models import Character
+import asyncpg
+import os
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    create_db_and_tables()
+    await init_db()
     yield
-
 app = FastAPI(lifespan=lifespan)
-
-@app.get("/")
-def read_root():
-    return {"status": "RPG Engine Online"}
-
 @app.post("/characters/")
-def create_character(character: Character, session: Session = Depends(get_session)):
-    session.add(character)
-    session.commit()
-    session.refresh(character)
-    return character
+async def create_character(character: Character):
+    conn = await asyncpg.connect(
+        user='postgres',
+        password=os.getenv('POSTGRES_PASSWORD'),
+        database='rpg_world',
+        host='localhost',
+        port=5432
+    )
+    try:
+        row = await conn.fetchrow('''
+                                  INSERT INTO characters (name, level, current_hp, max_hp, strength, dexterity, inventory, is_alive)
+                                  VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id;
+                                  ''', character.name, character.level, character.current_hp, character.max_hp,
+                                  character.strength, character.dexterity, character.inventory, character.is_alive)
 
-@app.get("/characters/")
-def read_characters(session: Session = Depends(get_session)):
-    characters = session.exec(select(Character)).all()
-    return characters
+        character.id = row['id']
+        return character
+    finally:
+        await conn.close()
